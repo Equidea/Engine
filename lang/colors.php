da53b537a12ca4ffe482b9bf0e8fd548b88fa725
Lisa Saalfrank <lsaalfrank@hof-university.de>
2016-05-25 17:25:15 +0200

    Add translations

---
+++ lang/colors.php
@@ -0,0 +1,30 @@
+<?php
+
+$colors = [
+
+    // English
+    'en' => [
+        1 => 'Black',
+        2 => 'Bay',
+        3 => 'Chestnut',
+        4 => 'Smoky Black',
+        5 => 'Buckskin',
+        6 => 'Palomino',
+        7 => 'Smoky Cream',
+        8 => 'Perlino',
+        9 => 'Cremello'
+    ],
+    
+    // German
+    'de' => [
+        1 => 'Rappe',
+        2 => 'Brauner',
+        3 => 'Fuchs',
+        4 => 'Erdbraun',
+        5 => 'Erdfarben',
+        6 => 'Isabell',
+        7 => 'Smoky Cream',
+        8 => 'Perlino',
+        9 => 'Cremello'
+    ]
+];
